440a410ca81e07389d33c9908e56fbf76074fc48
Eric Auel <eauel@vols.utk.edu>
2018-12-05 20:28:55 -0500

    Change TimeoutError to IOError to handle Python 2.7

--- tests/test_data_service.py
+++ tests/test_data_service.py
@@ -33,7 +33,7 @@ class TestDataServices(unittest.TestCase):
                     flag = False
                     break
             self.assertTrue(flag)
-        except (error_temp, TimeoutError):
+        except (error_temp, IOError):
             pass  # Catch error with attempting to test FTP functionality on Travis CI
 
         # Remove the downloaded files
